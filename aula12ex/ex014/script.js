526a1ca74626e3919031afc5ae2c14ec6229b041
Rafasfrancah <rafasfrancah@gmail.com>
2023-02-03 07:23:08 -0300

    Update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // Bom dia!
+        img.src = 'manha.png'
+        document.body.style.background = '#a99c68'
+    }else if (hora >= 12 && hora <= 18){
+        // Boa tarde!
+        img.src = 'tarde.png'
+        document.body.style.background = '#fed980'
+    } else{
+        // Boa noite!
+        img.src = 'noite.png'
+        document.body.style.background = '#081b1f'
+    }
+
+}
+
