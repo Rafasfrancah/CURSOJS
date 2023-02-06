ea7de0a8d912b12c1331f60a6ab1a17f5870e812
Rafael França <rafasfrancah@gmail.com>
2023-02-06 20:42:39 -0300

    Update

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,9 @@
-alert('Olá!')
+function Contar(){
+    var inicio = window.document.getElementById('txtinicio')
+    var fim = window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpasso')
+    
+
+
+
+}
