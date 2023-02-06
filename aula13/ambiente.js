45531075d0edf80b0e898a114c065b25becda5bd
Rafasfrancah <rafasfrancah@gmail.com>
2023-02-06 07:38:50 -0300

    Update

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+var contador = 1
+do {
+    console.log(`passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+
+
+
+
+
+/*
+var contador = 1
+while (contador <= 6){
+    console.log(`passo ${contador}`)
+    contador++
+}
+*/
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
