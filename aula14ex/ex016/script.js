4693c2ca5d87bec97d4f4f735208a43765ec383a
Rafasfrancah <rafasfrancah@gmail.com>
2023-02-07 07:32:43 -0300

    Update

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,9 +1,33 @@
-function Contar(){
-    var inicio = window.document.getElementById('txtinicio')
-    var fim = window.document.getElementById('txtfim')
-    var passo = window.document.getElementById('txtpasso')
+function contar(){
+    var inicio = window.document.getElementById("txtinicio")
+    var fim = window.document.getElementById("txtfim")
+    var passo = window.document.getElementById("txtpasso")
+    var resultado = window.document.getElementById("resultado")
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+
+    } else{
+        resultado.innerHTML = 'Contando:'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f){
+            for(var c = i; c <= f; c += p){
+                resultado.innerHTML += ` ${c} \u{1F449}`
+        }
+        resultado.innerHTML += `\u{1F3C1}`
+        
+        } else{
+            for(var c = i; c >= f; c -= p){
+                resultado.innerHTML += `${c} \u{1F449}`
+
+            }
+            resultado.innerHTML += `\u{1F3C1}`
+        }
+   
+
+   }
     
-
-
 
 }
