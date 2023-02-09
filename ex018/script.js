25fc699b80bf948b6f5957537c5fb01fcd6c2293
Rafasfrancah <rafasfrancah@gmail.com>
2023-02-09 13:52:09 -0300

    update

---
+++ ex018/script.js
@@ -0,0 +1,32 @@
+var num = window.document.querySelector('input#fnum')
+var lista = window.document.querySelector('input#flista')
+var resultado = window.document.querySelector('div#resultado')
+var valores = []
+
+function isnumero(n){
+    if(Number(n) >= 1 && Number(n) <=  100) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function inlista(n, l){
+    if(l.indexOf(Number(n)) != 1){
+        return true
+
+    } else{
+        return false
+    }
+
+}
+function adicionar(){
+    if(isnumero(num.value) && !inlista(num.value, valores)){
+        window.alert('Tudo ok')
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+
+} 
+}
