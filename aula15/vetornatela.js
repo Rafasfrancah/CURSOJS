ed3a1a582c3ebeaef95d2d7483a16c0e001dc16b
Rafasfrancah <rafasfrancah@gmail.com>
2023-02-09 06:48:23 -0300

    Update Array

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -9,16 +9,14 @@ console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
 console.log(valores[5])
-*/
-
 
 
 /*for(var pos = 0; pos < valores.length; pos++ ){
     console.log(`A posição ${pos} tem o valor${valores[pos]}`)
 }*/
 
-for(var pos in num){
-    console.log(num[pos])
+for(var pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
 
 
